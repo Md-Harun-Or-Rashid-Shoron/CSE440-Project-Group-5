fe368089659e2aee00f4afe5c68ab80ac3614b8b
Md-Harun-Or-Rashid-Shoron <hrshoron11@gmail.com>
2024-10-26 21:59:01 +0600

    Add files via upload

---
+++ SleepDisorderPrediction.py
@@ -0,0 +1,80 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+# dataset
+file_path = 'D:\CSE498R\Sleep_health_and_lifestyle_dataset.csv'
+data = pd.read_csv(file_path)
+
+# Label encoding
+label_encoders = {}
+for column in ['Gender', 'Occupation', 'BMI Category', 'Blood Pressure', 'Sleep Disorder']:
+    le = LabelEncoder()
+    data[column] = le.fit_transform(data[column])
+    label_encoders[column] = le
+
+# Split the data into training and testing sets
+X = data.drop('Sleep Disorder', axis=1)
+y = data['Sleep Disorder']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
+
+# Train Decision Tree 
+clf_dt = DecisionTreeClassifier()
+clf_dt.fit(X_train, y_train)
+
+# Predict on the test set with Decision Tree
+y_pred_dt = clf_dt.predict(X_test)
+
+# Calculate metrics for Decision Tree
+accuracy_dt = accuracy_score(y_test, y_pred_dt)
+precision_dt = precision_score(y_test, y_pred_dt, average='macro')
+recall_dt = recall_score(y_test, y_pred_dt, average='macro')
+f1_dt = f1_score(y_test, y_pred_dt, average='macro')
+
+print("Decision Tree Metrics:")
+print("Accuracy:", accuracy_dt)
+print("Precision:", precision_dt)
+print("Recall:", recall_dt)
+print("F1 Score:", f1_dt)
+
+# Train K-Nearest Neighbors
+clf_knn = KNeighborsClassifier(n_neighbors=5)
+clf_knn.fit(X_train, y_train)
+
+# Predict on the test set with KNN
+y_pred_knn = clf_knn.predict(X_test)
+
+# Calculate metrics for KNN
+accuracy_knn = accuracy_score(y_test, y_pred_knn)
+precision_knn = precision_score(y_test, y_pred_knn, average='macro')
+recall_knn = recall_score(y_test, y_pred_knn, average='macro')
+f1_knn = f1_score(y_test, y_pred_knn, average='macro')
+
+print("\nK-Nearest Neighbors Metrics:")
+print("Accuracy:", accuracy_knn)
+print("Precision:", precision_knn)
+print("Recall:", recall_knn)
+print("F1 Score:", f1_knn)
+
+# Train Random Forest
+clf_rf = RandomForestClassifier(n_estimators=100, random_state=42)
+clf_rf.fit(X_train, y_train)
+
+# Predict on the test set with Random Forest
+y_pred_rf = clf_rf.predict(X_test)
+
+# Calculate metrics for Random Forest
+accuracy_rf = accuracy_score(y_test, y_pred_rf)
+precision_rf = precision_score(y_test, y_pred_rf, average='macro')
+recall_rf = recall_score(y_test, y_pred_rf, average='macro')
+f1_rf = f1_score(y_test, y_pred_rf, average='macro')
+
+print("\nRandom Forest Metrics:")
+print("Accuracy:", accuracy_rf)
+print("Precision:", precision_rf)
+print("Recall:", recall_rf)
+print("F1 Score:", f1_rf)
